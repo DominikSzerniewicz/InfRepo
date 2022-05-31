9f62aa041bcd6307745fd4cebad43c7cecf2ee51
DominikSzerniewicz <dominik_szerniewicz@o2.pl>
2022-05-31 14:35:53 +0200

    all stuff

---
+++ fibonacci/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int f0 = 0;
+	int f1 = 1;
+	int n, f;
+
+	cout << "Ile wyrazów ciągu wypisać: ";
+	cin >> n;
+
+	for(int i=0; i<=n; ++i) {
+		if(i > 1) {
+			f = f0+f1;
+			f0 = f1;
+			f1 = f;
+		}
+		else {
+			f = i;
+		}
+
+		cout << f << endl;
+	}
+
+}
+
+
